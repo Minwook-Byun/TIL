3ccf5172399d6f642095bf48647632691bf73ce1
Boram <boram.byunminwook@gmail.com>
2021-09-04 19:30:04 +0900

    💄 updating background JS & images

--- Nomad/Final-Project/src/JS/background.js
+++ Nomad/Final-Project/src/JS/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "3.jpg", "4.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 
 const bgImage = document.createElement("img");
@@ -6,7 +6,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 bgImage.style.position = `absolute`;
     bgImage.style.left = `0`;
     bgImage.style.top = `0`;
-    bgImage.style.zIndex = `-1`
+    bgImage.style.zIndex = `-9999`
     bgImage.src = `img/${chosenImage}`;
     document.body.appendChild(bgImage)
 
