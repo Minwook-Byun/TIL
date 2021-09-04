c9aa882e7814d78c177d543bf086f44a82a4e4d5
Boram <boram.byunminwook@gmail.com>
2021-09-04 14:13:26 +0900

    ✨ Updating User-wheather section

---
+++ Nomad/Final-Project/src/JS/WeatherLocation.js
@@ -0,0 +1,22 @@
+const weather = document.querySelector("#weather .user-weather");
+const city = document.querySelector("#weather .user-city");
+const API_KEY = "94239409792267dd6736400083a10977";
+
+function onGeoWork(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoWork, onGeoError);
