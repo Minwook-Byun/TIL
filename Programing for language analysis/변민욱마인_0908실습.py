b207c0d321823432ba0afa94a5effa6acb6583f6
Boram <boram.byunminwook@gmail.com>
2021-09-08 17:46:07 +0900

    HUFS-언어분석을 위한 파이썬-0908과제

---
+++ Programing for language analysis/변민욱마인_0908실습.py
@@ -0,0 +1,25 @@
+'''
+# 문자열 조합
+animal = 'elephant'
+print('total:', animal.count('e'),'개')
+'''
+'''
+# random & shuffle 
+import random
+num = [3, 5, 7, 9]
+print(random.choice(num))
+random.shuffle(num)
+print(num)
+'''
+
+x = 12 
+y = 23
+packing = [12, 23]
+print(packing)
+[c1, c2] = packing
+print('c1:', c1)
+print('c2:', c2)
+
+a=['태희','비']
+b=['수지', '강다니엘']
+print(a+b)
