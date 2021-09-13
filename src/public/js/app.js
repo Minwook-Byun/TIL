9b1e85e900082feae1b56e9410a61fdd6f280c14
Boram <boram.byunminwook@gmail.com>
2021-09-13 21:36:49 +0900

    ✨ Sending chat to the backend

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("form")
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -15,3 +17,14 @@ socket.addEventListener("close", () => {
 setTimeout(() => {
     socket.send("hello from the browser!");
 }, 10000);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    // 프론트에서 백엔드로 보내주기 
+    console.log(input.value);
+    input.value= "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
