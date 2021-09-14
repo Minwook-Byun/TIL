997e6c45f9b20d2633871e28e11dfaf88da10456
Boram <boram.byunminwook@gmail.com>
2021-09-14 12:02:06 +0900

    ✨ Real-time chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,30 @@
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const nickForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#message")
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅");
+
 });
 
+function makeMessage(type, payload) {
+    const msg = {
+        type,
+        payload
+    };
+    return JSON.stringify(msg);
+}
+
+
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    // console.log("New message: ", message.data);
+    // 메세지를 받으면, 새로운 li를 만들어준다. 
+    const li = document.createElement("li");
+    // 메세지 데이타를 li안에 넣어준다 
+    li.innerText = message.data;
+    // li를 messagelist(ul)에 넣어준다. 
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -15,16 +32,27 @@ socket.addEventListener("close", () => {
 });
 
 setTimeout(() => {
+    // 백엔드로 보내는 메세지 
     socket.send("hello from the browser!");
 }, 10000);
 
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
     // 프론트에서 백엔드로 보내주기 
+    socket.send("new_message", input.value);
     console.log(input.value);
-    input.value= "";
+    input.value = "";
+
 }
 
+function handleNickSubmit(e) {
+    e.preventDefault();
+    const input = nickForm.querySelector("input")
+    socket.send(
+        makeMessage("nickname", input.value)
+    )
+}
+
+nickForm.addEventListener("submit", handleNickSubmit);
 messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -12,17 +12,29 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({
     server
 });
 
+const userSockets = [];
+
+// 여기서 백엔드에서 listen을 해주는 것.
+// 파폭, 브레이브, 크롬에도 작동을 하지만 
+// 각각의 브라우저 내에서만 소통을 하고 
+// 서로 다른 브라우저끼리는 소통이 되지 않는다. 
 wss.on("connection", (socket) => {
+    // firefox, brave가 연결될 때 여기에 소켓을 넣어준다. 
+    userSockets.push(socket); // [brave, firefox]
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
     socket.on("message", (message) => {
-        socket.send(message);
+        userSockets.forEach(aSocket => aSocket.send(message));
     });
 });
 
+// 근데 누가 접속했는지 모른다. 위 코드는 두번 작동
+// BRAVER와 연결될 때, 파이어폭스에서 연결될 때
+// 몇 명이 연결되어 있는지도 모른다. 
+
 server.listen(3000, handleListen);
+
