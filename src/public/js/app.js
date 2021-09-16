8237dde3b227ec9e54bc8ce3859afd631d25a694
Boram <boram.byunminwook@gmail.com>
2021-09-16 21:50:54 +0900

    ✨ zoomcloning

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,46 +1,43 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+// 알아서 socket.io를 실행하고 있는 서버를 찾아간다. 
+const socket = io();
+// 이미 room 기능이 있다. 
+const welcome = document.querySelector("#welcome")
+const room = document.querySelector("#room");
+const form = welcome.querySelector("form");
 
-function makeMessage(type, payload) {
-    const msg = {
-        type,
-        payload
-    };
-    return JSON.stringify(msg);
+room.hidden = true;
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3")
+    h3.innerHTML = `Room ${roomName}`;
+}
+let roomName;
+
+function addMessage(message) {
+    message.preventDefault();
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li")
+    li.innerHTML = message;
+    ul.appendChild(li); //ul에 li를 더한다! 순서 주의 
 }
 
-function handleOpen() {
-    console.log("Connected to Server ✅");
-}
+function handleRoomsubmit(e) {
+    e.preventDefault();
+    const userInput = form.querySelector("input");
+    socket.emit("enter_room", userInput.value, showRoom);
+    //어떤 것이든 emit할 수 있다. 또한 한 가지만 보낼 필요도 없고 object, function도 모두 가능하다. 프론트에서 보낼 수 있는데 같은 이름이어야 한다.
+    // 예전에는 먼저 object를 string으로 변환시키고 
+    // 그 다음에 string을 전송해야 했었다. socketIO는 그럴 필요가 없다. 
+    // argumnet는 얼마든지 넣을 수 있지만 백엔드가 끝났다는 function을 넣으려면 가장 마지막에 넣어야 한다. 
+    roomName = userInput.value;
+    userInput.value = ""
+};
 
-socket.addEventListener("open", handleOpen);
+form.addEventListener("submit", handleRoomsubmit);
 
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
 
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server ❌");
-});
-
-function handleSubmit(event) {
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    input.value = "";
-}
-
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
-
-// api이기 때문에 string만 보내야 한다. 
+socket.on("welcome", () => {
+    addMessage("someone joined!")
+})
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import http from "http";
-import WebSocket from "ws";
+import {
+    Server
+} from "socket.io";
+// import WebSocket from "ws";
 import express from "express";
 import {
     PassThrough
@@ -15,40 +18,58 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({
-    server
-});
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"]="익명의 사용자";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        const parsedMessage = JSON.parse(message);
-        switch (parsedMessage.type) {
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${parsedMessage.payload}`.toString('utf-8')));
-            case "nickname":
-                socket["nickname"] = parsedMessage.payload;
-                break;
-        }
-
-        // if (parsedMessage.type === "new_message") {
-        //     sockets.forEach((aSocket) => aSocket.send(parsedMessage.payload.toString('utf-8'))); //payload는 지금 입력해주는 값 
-        // } //어떤 메세지를 받는지 구분을 시켜주고 있다. 
-        // else if(parsedMessage.type === "nickname") {
-        //     console.log(parsedMessage.payload);
-        // }
-
+wsServer.on("connection", (socket) => {
+    socket.onAny((e) => {
+        console.log(`socket Event: ${e}`)
     });
-});
+    socket.on("enter_room", (roomName, done) => {
+        console.log(socket.rooms);
+        socket.join(roomName);
+        console.log(socket.rooms); //어느 room에 있는가 
+        done(); // done은 여기서 실행하는 것이 아니라 프론트에 있는 함수로 가서 실행해주는 것 보.안.문.제(실행버튼을 눌러준다)
+        socket.to(roomName).emit("welcome") //모든 사람들에게 welcome을 실행해준다. 프론트에서 이걸 반응하도록 만들어준다.
+    });
+    // 뒤에 원하는 event를 넣기만 하면 된다. 
+})
 
-server.listen(3000, handleListen);
+// client에게도 설치하게 해줘야함. 웹소켓은 socket.io와 호환이 되지 않는다. 
+// that's why they give this url
+
+// const wss = new WebSocket.Server({
+//     server
+// });
+
+// function onSocketClose() {
+//     console.log("Disconnected from the Browser ❌");
+// }
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"]="익명의 사용자";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (message) => {
+//         const parsedMessage = JSON.parse(message);
+//         switch (parsedMessage.type) {
+//             case "new_message":
+//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${parsedMessage.payload}`.toString('utf-8')));
+//             case "nickname":
+//                 socket["nickname"] = parsedMessage.payload;
+//                 break;
+//         }
+
+//         // if (parsedMessage.type === "new_message") {
+//         //     sockets.forEach((aSocket) => aSocket.send(parsedMessage.payload.toString('utf-8'))); //payload는 지금 입력해주는 값 
+//         // } //어떤 메세지를 받는지 구분을 시켜주고 있다. 
+//         // else if(parsedMessage.type === "nickname") {
+//         //     console.log(parsedMessage.payload);
+//         // }
+
+//     });
+// });
+httpServer.listen(3000, handleListen);
