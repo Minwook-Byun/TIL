40a839fe073722e07dda461ca15bb63b762db8fa
Boram <boram.byunminwook@gmail.com>
2021-09-17 22:46:49 +0900

    🐛 app.post 중복 문제 해결

--- 서버만들기/server.js
+++ 서버만들기/server.js
@@ -7,8 +7,7 @@ app.use(express.urlencoded({
 const MongoClient = require('mongodb').MongoClient
 
 
-// 누군가가 /pet으로 방문을 하면 
-// pet관련된 안내문을 띄워주자 
+
 
 
 var db; //변수 하나 필요
@@ -16,16 +15,11 @@ MongoClient.connect('mongodb+srv://admin:Lifesecurity!@cluster0.xen2v.mongodb.ne
     if (에러) return console.log(에러)
     db = client.db('todoapp');
 
-    // db.collection('post').insertOne({
-    //     이름:'보람',
-    //     감상평: '너무 좋은 전시 잘 봤습니다.'
-    // }, function (에러, 결과) {
-    //     console.log('저장완료');
-    // });
-
     app.post('/add', function (req, res) {
         res.send('전송완료!');
-        db.collection('post').insertmany({
+        console.log(req.body.title);
+        console.log(req.body.date);
+        db.collection('post').insertOne({
                 title: req.body.title,
                 date: req.body.date
             }),
@@ -61,12 +55,6 @@ app.get('/write', (요청, 응답) => {
 
 // 어떤사람이 /add 경로로 post요청을 하면 .. ??을 해주세요 
 
-app.post('/add', (req, res) => {
-    res.send('전송완료');
-    console.log(req.body.title);
-    console.log(req.body.date);
-    // DB에 저장해주세요
-})
 
 // input 에 적은 정보는 어딨지? => req에 저장되어 있음 => body parser
 // 데이터 해석을 도와준다. 
