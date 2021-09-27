b1ceed7de3ffeb3b875ff1a6f4495bc987170f1a
Boram <boram.byunminwook@gmail.com>
2021-09-27 20:37:10 +0900

    react 공부중

--- ZeroBase/begin-react/src/inputSample.js
+++ ZeroBase/begin-react/src/inputSample.js
@@ -1,24 +1,41 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 
 function InputSample() {
-    const [text, setText] = useState('');
-    
-    const onChange = (e) =>{
-        setText(e.target.value)
-        // e.target은 현재 선택된 DOM을 불러올 수 있고 
-        // 거기에 value로 input에 값을 가져올 수 있다. 
-    }
+    const [inputs, setInputs] = useState({
+        name:'',
+        nickanme:'',
+// 여러개를 받을 때는 함수도 여러개 만들어야 할까? no! 객체로 관리해주자 
+    });
+    const {name, nickanme} = inputs;
 
-    const OnRest = () => {
-        setText('');
+    const onChange = (e) =>{
+        const {name, value} = e.target;
+        const nextInputs= ({
+            ...inputs, //기존의 내용이 복사된다 
+            [name]:value, 
+        });
+    };
+// 불변성을 지킨다. 객체 상태를 업데이트를 할 때는 
+// 기존의 값을 복사하고 그것에 덮어씌우고 새로운 상태로 만들어줘야 한다.
+// 그래야 상태가 업데이트되었다는 것을 알게 되고 필요한 렌더링을 해준다. 
+const nameInput = useRef();
+    // 이렇게 호출해주고 => 원하는 dom에 설정을 해주고,  => 만들어준값 + current
+const OnRest = () => {
+        setInputs({
+            name:'',
+            nickanme:'',
+        });
+        nameInput.current.focus();
+        // 초기화를 실행햇을 때 
     }
     return(
         <div>
-            <input onChange={onChange} value={text}/>
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={useRef}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickanme}/>
             <button onClick={OnRest}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+                이름(닉네임)
             </div>
         </div>
     )
