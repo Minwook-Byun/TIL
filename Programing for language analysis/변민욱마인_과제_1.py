7cdae2fbde33a8487cebeec60972e2a58ec6a7b4
Boram <boram.byunminwook@gmail.com>
2021-10-01 20:33:34 +0900

    ⚡️

---
+++ Programing for language analysis/변민욱마인_과제_1.py
@@ -0,0 +1,28 @@
+# 52p. Quiz 4.1 
+# (1) sum ()함수 완성
+# def sum(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum += i
+#     return sum 
+# print(sum(10))
+
+# (2)팩토리얼 계산
+# def factorial(n):
+#     result = 1;
+#     for i in range(1,n):
+#         result *= i+1
+#     return result
+
+# print(factorial(3))
+
+# 69p. Quiz 5.3
+# (1)
+# animal = 'frog'
+# print(animal[1:4:2])
+# 결과 : rg
+
+# (2)
+# animal = 'elephant'
+# print(animal[1:7:2])
+# 결과 : lpa
