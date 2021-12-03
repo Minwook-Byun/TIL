bb7adbab355c4b0d4ec6e38adb3c8d9c9305b60b
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2021-12-03 11:38:35 +0900

    TIL_MAXHEAP_delete

--- Data structure/Maxheap.py
+++ Data structure/Maxheap.py
@@ -17,3 +17,25 @@ class MaxHeap:          #최대 힙 클래스
             self.heap[i] = self.Parent(i) #부모를 끌어 내림 
             i = i // 2 #i를 부모의 인덱스로 올림 
         self.heap[i] =n #마지막 위치에 n 삽입
+        
+    def delete(self):
+        parent = 1
+        child =2 
+        if not self.isEmpty():
+            root = self.heap[1] #삭제할 루트를 복사해 둠 
+            last = self.heap[self.size()] #마지막 노드 
+            
+            while (child <= self.size()): #마지막 노드 이전까지 
+                #만약 오른쪽 노드가 더 크면 child를 1 증가(기본은 왼쪽 노드)
+                if child < self.size() and self.left(parent) < self.Right(parent):
+                    child += 1
+                if last >= self.heap[child]: # 더 큰 자식이 더 작으면 
+                    break                    # 삽입 위치를 찾음. down-heap 종료 
+                self.heap[parent] = self.heap[child] #아니면 down - heap 계속 
+                parent = child 
+                child *= 2;
+                
+            self.heap[parent] = last #맨 마지막 노드를 parent 위치에 복사 
+            self.heap.pop(-1) #맨 마지막 노드 삭제 
+            return root #저장해두었던 루트를 반환
+        
