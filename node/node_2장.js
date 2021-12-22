3f1e72500a487c6a2198a216f90e0663c4c1386f
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2021-12-22 19:37:30 +0900

    [node]2장_프로미스이전

--- node/node_2장.js
+++ node/node_2장.js
@@ -72,3 +72,168 @@ function not1(x) {
 }
 
 const not2 = x => !x;
+
+var relationships = {
+    name : 'zero',
+    friends: ['nero', 'hero', 'xero'],
+    logFri : function() {
+        var that = this;
+        this.friends.forEach(function(friend){
+            console.log(that.name, friend);
+        });
+    },
+};
+
+relationships.logFri()
+//================================================================
+
+const relationship2 = {
+    name : 'zero', 
+    friends: ['nero', 'hero', 'xero'],
+    logFri() {
+        this.friends.forEach(friend => {
+            console.log(this.name, friend);
+        });
+    },
+};
+
+relationship2.logFri();
+
+//===========================================================================
+// 구조분해 할당 
+var candyMachine = {
+    status: {
+        name: 'node',
+        count: 5,
+    },
+    getCandy: function() {
+        this.status.count -- 
+        return this.status.count;
+    },
+};
+
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+
+const candyMachine = {
+    status: {
+        name: `node`,
+        count: 5,
+    },
+    getCandy(){
+        this.status.count--;
+        return this.status.count;
+    },
+};
+
+const {getCandy, status: {count} } = candyMachine;
+
+var array = ['node.js', {}, 10, true]
+var node = array[0];
+var obj = array[1];
+var bool = array[3];
+
+const array = ['node.js', {}, 10, true]
+const [node, obj, bool] = array 
+
+// ===========================================================
+var Human = function(type){
+    this.type = type || 'human'; 
+};
+
+Human.isHuman = function(human){
+    return human instanceof Human;
+}
+
+Human.prototype.breathe = function() {
+    alert('haaaaaaaaaaaaam')
+}
+
+var BORAM = function(type, firstName, lastName){
+    Human.apply(this, arguments);
+    this.firstName = firstName;
+    this.lastName = lastName;
+};
+
+BORAM.prototype = Object.create(Human.prototype);
+BORAM.prototype.constructor = BORAM; // 상속하는 부분 
+BORAM.prototype.sayName = function() {
+    alert(this.firstName + ' ' + this.lastName);
+};
+var OldBoram = new BORAM('human', 'BORAM', "I");
+Human.isHuman(OldBoram);
+
+// ================================
+class Human {
+    constructor(type = 'human'){
+        this.type = type;
+    }
+
+    static isHuman(human){
+        return human instanceof Human;
+    }
+
+    breathe() {
+        alert("haaaaaaaaaaam")
+    }
+}
+
+class Boram extends Human {
+    constructor(type, firstName, lastName){
+        super(type);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    sayName(){
+        super.breathe();
+        alert(`${this.firstName} ${this.lastName}`);
+    }
+}
+
+const newBoram = new Boram('human', 'Boram', 'chan');
+Human.isHuman(newBoram)
+
+//========================================
+const condition = true;
+const promise = new Promise((resolve, reject) => {
+    if(condition) {
+        resolve('성공');
+    } else {
+        reject('실패');
+    }
+});
+
+promise
+  .then((message) => {
+      console.log(message); // 성공(resolve)한 경우 실행
+  })
+  .catch((err) => {
+      console.log(error); // 실패(reject)한 경우 실행 
+  })
+  .finally(() => {
+      // 끝나기 전에 무조건 한 번은 실행
+      console.log('무조건')
+  }); 
+
+// =================================================================
+promise
+.then((message) => {
+    return new Promise((resolve, reject) => {
+        resolve(message);
+    });
+})
+.then((message2) => {
+    console.log(message2);
+    return new Promise((resolve, reject) => {
+        resolve(message2);
+    });
+})
+.then((message3) => {
+    console.log(message3);
+})
+.catch((error) => {
+    console.error(error);
+})
+
+//===============================
