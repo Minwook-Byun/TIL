43871ae8531209c70e7531adc9b6a3391333c421
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2021-12-23 18:04:29 +0900

    [node]_2장_require실습

---
+++ node/node_3장/3.4노드내장객체/노드기능.js
@@ -0,0 +1,12 @@
+// console.log(__filename); //c:\coding\TIL\node\node_3장\3.4노드내장객체\노드기능.js
+// console.log(__dirname); //c:\coding\TIL\node\node_3장\3.4노드내장객체
+
+// console.log(this); //{} 
+// console.log(this === module.exports) //true 
+// console.log(this === exports) // true
+
+// function WhatIsThis() {
+//     console.log('function', this === exports, this === global); 
+// }
+
+// WhatIsThis(); //function false true
