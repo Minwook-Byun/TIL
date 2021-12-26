543c50bb928bb5ac803a6b73a506d280687ffa5b
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2021-12-26 11:01:42 +0900

    [node]_3장_fsDelete

---
+++ node/node_3장/3.6 파일시스템접근하기/fsDelete.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises;
+
+fs.readdir('./folder')
+    .then((dir) => {
+        console.log('폴더내용확인', dir);
+        return fs.unlink('./folder/newfile.js');
+    })
+    .then(() => {
+        console.log('파일 삭제 성공');
+        return fs.rmdir('./folder');
+    })
+    .then(() => {
+        console.log('폴더 삭제 성공')
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    // 폴더내용확인 [ 'newfile.js' ]
+    // 파일 삭제 성공
+    // 폴더 삭제 성공
+    
