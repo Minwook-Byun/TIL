c8b1b190c7842d476c02112eb9d16d6fe44c22fb
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-02 16:36:15 +0900

    [node]_6장_cookieParser

---
+++ node/node_6장/6.2_middle/cookie_parser.js
@@ -0,0 +1,6 @@
+res.cookie('name','boram', {
+    expires: new Date(Date.now() + 900000),
+    httpOnly: true, 
+    secure: true,
+});
+res.clearCookie('name', 'boram', {httpOnly: true, secure: true});
