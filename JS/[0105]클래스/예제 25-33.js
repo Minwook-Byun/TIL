ecec5c350667b6ef3679309e11b59a7802af5f9a
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-05 17:53:47 +0900

    [JS딥다이브]클래스_public

---
+++ JS/[0105]클래스/예제 25-33.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(name) {
+        //인스턴스 프로퍼티 
+        this.name = name; // name 프로퍼티는 public
+    }
+}
+
+const me = new Person('Boram')
+console.log(me.name) //Boram
+
+
+
+
