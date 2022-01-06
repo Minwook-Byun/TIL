49b0036e21982ab8610ffa5f4c350887cbf7757c
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-06 13:21:30 +0900

    [JS딥다이브]화살표함수

---
+++ JS/[0106]ES6함수의추가기능/예제26-11.js
@@ -0,0 +1,13 @@
+//예제 26-11
+const multiply = (x, y) => x * y;
+console.log(multiply(2, 3)); //6
+
+//예제 26-12
+const arrow = (x, y) => { ... }; 
+
+//예제 26-13
+const arrow = x => {...}
+
+//예제 26-14
+const arrow = () => { ...}
+
