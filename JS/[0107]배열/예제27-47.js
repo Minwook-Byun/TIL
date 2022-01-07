3d30a5c16b7d2044a39ebc8dd93f2e48fd6450a0
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-07 18:59:51 +0900

    [JS딥다이브] cocat&스프레드문법

--- JS/[0107]배열/예제27-47.js
+++ JS/[0107]배열/예제27-47.js
@@ -8,4 +8,3 @@ const spreadArray = ['Hankook', 'university']
 // ES6 스프레드 문법 
 const newArray = [...spreadArray,'of', 'foregin','sutudies']
 console.log(newArray); //[ 'Hankook', 'university', 'of', 'foregin', 'sutudies' ]
-
