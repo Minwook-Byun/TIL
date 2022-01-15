db4773f347f68d71a4c103dca153f7031262b44f
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-15 22:48:02 +0900

    [타입스크립트]오버로딩

---
+++ typescript/[0115]아이템3/item3-7.js
@@ -0,0 +1,3 @@
+// 오버로딩 
+function add(a, b) { return a + b; }
+function add(a, b) { return a + b; }
---
+++ typescript/[0115]아이템3/item3-7.ts
@@ -0,0 +1,7 @@
+// 오버로딩 
+function add(a: number, b: number) {return a+b;}
+function add(a: number, b: number) {return a+b;}
+
+// 타입 수준에서만 
+function add(a:number, b:number):number
+function add(a:string, b:string):string 
