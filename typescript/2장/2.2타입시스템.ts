6ddbae54908d8e9de6bd5ea9b38304f62bb03695
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-16 19:04:27 +0900

    [타입스크립트]타입시스템

---
+++ typescript/2장/2.2타입시스템.ts
@@ -0,0 +1,11 @@
+let a:number = 1;             //a는 number 
+let b:string = 'hello world'; //b는 string
+let c:boolean[] = [true,false]; // c는 boolean 배열 
+
+// let a = 1             //a는 number 
+// let b = 'hello world' //b는 string
+// let c = [true,false] // c는 boolean 배열 
+
+// 3 + [1];
+
+(3).toString() + [1].toString() 
