1d98aa70e86d3cb9e55b7f1eb4bb37df4406262c
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-16 19:08:38 +0900

    [typescript]type_Symbol

---
+++ typescript/3장/3장_symbol.ts
@@ -0,0 +1,5 @@
+let symbol_a = Symbol('a')
+let symbol_b = Symbol('b')
+var symbol_c = symbol_a === symbol_b
+// let symbol_d = symbol_a + 'x' 
+//The '+' operator cannot be applied to type 'symbol'.ts(2469)
