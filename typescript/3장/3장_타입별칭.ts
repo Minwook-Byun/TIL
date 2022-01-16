1c3d5e68560d2e9f9f7189613b7b1d0348b34215
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-16 19:07:31 +0900

    [typescript]타입별칭

---
+++ typescript/3장/3장_타입별칭.ts
@@ -0,0 +1,18 @@
+type Age = number 
+type _Person = {
+    name: string
+    age : Age 
+}
+
+let age: Age = 55
+let student : _Person ={
+    name : 'boram',
+    age: age
+}
+
+let _age = 55
+let _student :_Person = {
+    name:'boram',
+    age: age
+}
+
