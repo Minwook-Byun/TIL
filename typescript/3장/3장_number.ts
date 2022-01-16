65756ccac5b30805f74e4eae594c02065e8673da
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-16 19:08:08 +0900

    [typescript]type_number

---
+++ typescript/3장/3장_number.ts
@@ -0,0 +1,7 @@
+let num_a = 1234
+var num_b = Infinity * 0.1
+const num_c = 5678
+let num_d = num_a < num_b //boolean
+let num_e:number = 1000
+let f:26.182 = 26.182
+// let g: 26.182 = 10 // Type '10' is not assignable to type '26.182'
