28af46d158f85dec3b433d4bc1c70a3d080c46ae
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-17 22:50:10 +0900

    [react] inline styling in the react

---
+++ 리액트를다루는기술/myapp/src/2장 JSX/JSX예제1-6.js
@@ -0,0 +1,15 @@
+function App() {
+  const name = '리액트';
+  return (
+    <div
+      style={{
+        backgroundColor: '#FFFFFF',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+      }}
+    >
+      {name}
+    </div>
+  );
+}
