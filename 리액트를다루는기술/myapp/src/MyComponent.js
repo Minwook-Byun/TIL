4951bd8ceeed5907673b7d6ffc4ed93443b16a63
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-18 23:36:27 +0900

    [react]Using isRequired in the propTypes

--- 리액트를다루는기술/myapp/src/MyComponent.js
+++ 리액트를다루는기술/myapp/src/MyComponent.js
@@ -1,5 +1,22 @@
-const Mycomponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+import PropTypes from 'prop-types';
+
+const MyComponent = ({ name, favoriteNum, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children의 값은 {children} <br />
+      제가 좋아하는 숫자는 {favoriteNum}입니다
+    </div>
+  );
 };
 
-export default Mycomponent;
+MyComponent.defaultProps = {
+  name: '⭐ 유저의 기본 이름 ⭐ ',
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNum: PropTypes.number.isRequired,
+};
+
+export default MyComponent;
