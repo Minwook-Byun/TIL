b98a7aae4552e4cf33be239d861cf70595576925
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-19 14:05:28 +0900

    [react] Using the setStates

--- 리액트를다루는기술/myapp/src/evenPractice.js
+++ 리액트를다루는기술/myapp/src/evenPractice.js
@@ -16,6 +16,10 @@
 import { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    message: ' ',
+  };
+
   render() {
     return (
       <div>
@@ -24,10 +28,22 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해보세요🙋"
+          value={this.state.message}
           onChange={(e) => {
-            console.log(e.target.value);
+            this.setState({
+              message: e.target.value,
+            });
           }}
         />
+        <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({ message: '' });
+          }}
+        >
+          {' '}
+          확인{' '}
+        </button>
       </div>
     );
   }
