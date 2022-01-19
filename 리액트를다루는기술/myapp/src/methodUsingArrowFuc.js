7bdcb4abd7456cd4db9aa9a640948eafb60f18e2
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-19 23:41:01 +0900

    [react] plz use the arrow function for the bind

---
+++ 리액트를다루는기술/myapp/src/methodUsingArrowFuc.js
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+
+class EventPractice extends Component {
+  state = {
+    message: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({ message: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습하기</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해보세요🙋"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
