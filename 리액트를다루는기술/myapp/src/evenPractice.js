fbe0cb25f21233af466defaffdaf6224f90cdffd
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-19 13:54:09 +0900

    [react] e.target.value 사용

--- 리액트를다루는기술/myapp/src/evenPractice.js
+++ 리액트를다루는기술/myapp/src/evenPractice.js
@@ -1,11 +1,33 @@
+// import { Component } from 'react';
+
+// class EventPractice extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {' '}
+//         <h1>이벤트 실습</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default EventPractice;
+
 import { Component } from 'react';
 
 class EventPractice extends Component {
   render() {
     return (
       <div>
-        {' '}
-        <h1>이벤트 실습</h1>
+        <h1> 이벤트 연습하기</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해보세요🙋"
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
       </div>
     );
   }
