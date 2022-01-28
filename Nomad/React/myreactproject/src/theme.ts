9679967c4025ddae79ed7625a49c3d046563a6c0
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-28 12:39:33 +0900

    migration to the Typescript

--- Nomad/React/myreactproject/src/theme.ts
+++ Nomad/React/myreactproject/src/theme.ts
@@ -1,7 +1,7 @@
-import * as styledComponents from "styled-components";
+import { DefaultTheme } from "styled-components";
 
-export const theme: styledComponents.DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
 };
