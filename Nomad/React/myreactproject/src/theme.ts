5d46541f044819a2e6158f37b586f728ee818773
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-28 10:23:37 +0900

    🎉 [NoM] Begin : Begin the coin project

---
+++ Nomad/React/myreactproject/src/theme.ts
@@ -0,0 +1,7 @@
+import * as styledComponents from "styled-components";
+
+export const theme: styledComponents.DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
--- React_Firebase/myapp/src/App.js
+++ React_Firebase/myapp/src/App.js
@@ -1,18 +1,63 @@
-import useState from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let name = "보람";
-  const handleButtonClick = () => {
-    name = "minwook";
-    console.log(name);
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([
+    { title: "welcome to the React", id: 1 },
+    { title: "also welcom to the ES6", id: 2 },
+    { title: "완강까지 화이팅~~~", id: 3 },
+  ]);
+  const handleButtonClick = (id) => {
+    setEvents((preEvents) => {
+      return preEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
   };
-
+  // setEvents 안에서 event를 쓰는 것은 badpractice
   return (
-    <div className="App">
-      <h1> 멋쟁이 {name}처럼</h1>
-      <button onClick={handleButtonClick}>이름변경</button>
-    </div>
+    <>
+      {showEvents && (
+        <div>
+          <button
+            onClick={() => {
+              setShowEvents(false);
+            }}
+          >
+            숨기기
+          </button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button
+            onClick={() => {
+              setShowEvents(true);
+            }}
+          >
+            더보기
+          </button>
+        </div>
+      )}
+
+      {showEvents && // 요것 자체가 false이면 아예 다음 것 평가 자체를 하지 않는다
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index}-{event.title}
+            </h2>
+            <button
+              onClick={() => {
+                handleButtonClick(event.id);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        ))}
+    </>
   );
 }
 
