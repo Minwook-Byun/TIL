b233cab559bc491df2dc197977833def01deca0b
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-29 16:24:54 +0900

    Learn : two-way connection using values

--- React_Firebase/myapp/src/NewEventForm.js
+++ React_Firebase/myapp/src/NewEventForm.js
@@ -9,31 +9,45 @@ const Neweventform = () => {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
 
+  const resetForm = () => {
+    setTitle(" ");
+    setDate(" ");
+  };
+
   return (
     <div>
       <form className="new-evnet-form" />
-      <label>
+      <label className="new-evnet-form-label">
         <span>Event Title:</span>
         <input
           type="text"
           onChange={(e) => {
             setTitle(e.target.value);
           }}
+          value={title}
         />
       </label>
 
-      <label>
+      <label className="new-event-form-evnet">
         <span>Event Date:</span>
         <input
           type="date"
           onChange={(e) => {
             setDate(e.target.value);
           }}
+          value={date}
         />
       </label>
       <button> submit </button>
       <p> title: {title}</p>
       <p> date: {date}</p>
+      <button
+        onClick={() => {
+          resetForm();
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
