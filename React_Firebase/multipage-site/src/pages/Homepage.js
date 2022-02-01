1b2c78365948a4cef2f7df1b297d611a9914bad6
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-01 13:13:37 +0900

    💄 구현 : data 받아오고 useFetch 통해 뿌리기

--- React_Firebase/multipage-site/src/pages/Homepage.js
+++ React_Firebase/multipage-site/src/pages/Homepage.js
@@ -1,10 +1,29 @@
+import { useFetch } from "../hooks/useFetch";
 import React from "react";
 
+// 스타일링
+import "./Home.css";
+
 const Homepage = () => {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
+
   return (
-    <div>
+    <div className="home">
       <h1>홈페이지</h1>
       <p>새해 복 많이 받으셈!</p>
+      {isPending && <div>로딩 중임!</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
     </div>
   );
 };
