d1673b71849e7a4d28e6aad3cba98b6cfbb3a25d
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-01 19:35:08 +0900

    추가 : Home 화면 로딩과 에러 처리

--- React_Firebase/order_recipe/src/pages/home/Home.js
+++ React_Firebase/order_recipe/src/pages/home/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    data: menus,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/recipes");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error},에러났음 ㅇㅅㅇ</p>}
+      {isPending && <div className="loading"> 로딩중....</div>}
+      {menus && <RecipeList menus={menus} />}
+    </div>
+  );
 };
 
 export default Home;
