144162ff3eaf2af03635d1547964364545bdbfe2
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-01 20:41:55 +0900

    ✨ 추가 : 레시피 세부사항 추가 구현

--- React_Firebase/order_recipe/src/pages/recipe/Recipe.js
+++ React_Firebase/order_recipe/src/pages/recipe/Recipe.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
-const Recipe = () => {
-  return <div>Recipe</div>;
-};
+import "./Recipe.css";
+
+function Recipe() {
+  const { id } = useParams();
+  const url = " http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+
+  return (
+    <div className="recipe">
+      {error && <p>에러났음!</p>}
+      {isPending && <span className="loading">로딩중</span>}
+      {recipe && (
+        <>
+          <h2 className="title">{recipe.title}</h2>
+          <p> 조리시간 : {recipe.cookingTime}</p>
+          <p className="method"> 조리방법: {recipe.method} </p>
+          <ul>
+            {recipe.ingredients.map((ing, i) => (
+              <li key={i}>
+                재료{i + 1} : .{ing}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
 
 export default Recipe;
