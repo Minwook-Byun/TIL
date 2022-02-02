b07f80e8b20550ced5a0f2bd4c31b70809acd964
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-02 22:14:46 +0900

    추가 : create form 내부 ingredient

--- React_Firebase/order_recipe/src/pages/create/Create.js
+++ React_Firebase/order_recipe/src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // styles
 import "./Create.css";
@@ -8,11 +8,23 @@ export default function Create() {
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
   const [newIngredient, setNewIngredient] = useState("");
-  const [ingredients, setIngredients] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    }
+    setNewIngredient("");
+    ingredientInput.current.focus();
   };
 
   return (
@@ -20,7 +32,7 @@ export default function Create() {
       <h2 className="page-title">Add a New Recipe</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          <span>레시피를 추가하시겠음?:</span>
+          <span>Recipe title:</span>
           <input
             type="text"
             onChange={(e) => setTitle(e.target.value)}
@@ -30,7 +42,28 @@ export default function Create() {
         </label>
 
         <label>
-          <span>재료 :</span>
+          <span>Recipe Ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{" "}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
+
+        <label>
+          <span>Recipe Method:</span>
           <textarea
             onChange={(e) => setMethod(e.target.value)}
             value={method}
@@ -39,7 +72,7 @@ export default function Create() {
         </label>
 
         <label>
-          <span>조리시간:</span>
+          <span>Cooking time (minutes):</span>
           <input
             type="number"
             onChange={(e) => setCookingTime(e.target.value)}
