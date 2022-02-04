872966a4d5dd325078fc4a7d6ad19ba6a24a54f2
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-04 09:49:16 +0900

    추가 : 회원가입 기본형태 추가

--- React_Firebase/my-money/src/pages/signup/SignUP.js
+++ React_Firebase/my-money/src/pages/signup/SignUP.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./SignUP.module.css";
 
 const Signup = () => {
-  return <div>회원가입</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
+  };
+
+  return (
+    <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      <label>
+        <span>이름</span>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        <span> 이메일 </span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        <span> 비밀번호 </span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button className="btn"> 회원가입 </button>
+    </form>
+  );
 };
 
 export default Signup;
