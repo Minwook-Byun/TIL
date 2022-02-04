5547dc1c975e7b76d8ec8f533f8fb6f1aa2f65b7
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-04 09:09:01 +0900

    🐛 라우팅 관련 버그 해결

--- React_Firebase/my-money/src/App.js
+++ React_Firebase/my-money/src/App.js
@@ -3,23 +3,23 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // 컴포넌트
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import SignUP from "./pages/signup/SignUP";
+import Signup from "./pages/signup/SignUP";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-
           <Route>
             <Login path="/login" />
           </Route>
-
-          <Route path="/signup">
-            <SignUP />
+          <Route path="/회원가입">
+            <Signup />
           </Route>
         </Switch>
       </BrowserRouter>
