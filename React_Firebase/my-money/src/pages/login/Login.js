715276c5abede6d4aec337025fdcf3c44789a3a7
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-04 09:48:59 +0900

    추가 : 로그인 기본 형태 추가

--- React_Firebase/my-money/src/pages/login/Login.js
+++ React_Firebase/my-money/src/pages/login/Login.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import styles from "./Login.module.css";
 
 const Login = () => {
-  return <div>로그인임</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
+      <h2>로그인</h2>
+      <label>
+        <p>이메일:</p>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>비밀번호</span>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      </label>
+
+      <button className="btn"> 로그인 </button>
+    </form>
+  );
 };
 
 export default Login;
