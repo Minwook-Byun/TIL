73afb19bfcfa0dd0144f190936d7916210811e2a
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-04 09:48:34 +0900

    추가 : navbar 추가 & 스타일

---
+++ React_Firebase/my-money/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}> 뱅큽 샐러드</li>
+
+        <li>
+          <Link to="/login"> 로그인 </Link>
+        </li>
+        <li>
+          <Link to="/signup"> 회원가입 </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
