253ecc310c64979665ba07cf2639a8f8219da816
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-05 23:18:00 +0900

    추가 : conditional view 구현

--- React_Firebase/my-money/src/components/Navbar.js
+++ React_Firebase/my-money/src/components/Navbar.js
@@ -1,22 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>
           <Link to="/">뱅큽 샐러드</Link>
         </li>
-
-        <li>
-          <Link to="/login"> 로그인 </Link>
-        </li>
-        <li>
-          <Link to="/signup"> 회원가입 </Link>
-        </li>
+        {!user && (
+          // user가 null(false 라면)
+          <>
+            <li>
+              <Link to="/login"> 로그인 </Link>
+            </li>
+            <li>
+              <Link to="/signup"> 회원가입 </Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <p>{user.displayName}</p>
+            <li>
+              <button className="btn" onClick={logout}>
+                로그아웃
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
