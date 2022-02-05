48d875c771e4926cfc6779c918f21fdb29ad0e9b
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-05 23:20:24 +0900

    cleaning function 추가

--- React_Firebase/my-money/src/hooks/useSignup.js
+++ React_Firebase/my-money/src/hooks/useSignup.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  //   폼에서 받아온 3 데이터 처리하기
-  const signup = async ({ email, password, username: displayName }) => {
+  const signup = async (email, password, displayName) => {
     setError(null);
     setIsPending(true);
 
@@ -18,7 +18,6 @@ export const useSignup = () => {
         email,
         password
       );
-      console.log(res.user);
 
       if (!res) {
         throw new Error("Could not complete signup");
@@ -27,16 +26,24 @@ export const useSignup = () => {
       // add display name to user
       await res.user.updateProfile({ displayName });
 
+      // dispatch login action
       dispatch({ type: "LOGIN", payload: res.user });
 
-      setIsPending(false);
-      setError(null);
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
   return { signup, error, isPending };
 };
