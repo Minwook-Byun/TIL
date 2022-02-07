8fb5a5341635a7ce5587ccdcd12c6ea35cc87920
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-07 21:32:30 +0900

    ✏️혼자서 해보기 : 라우터 설정

--- React_Firebase/kakyeboo/src/App.js
+++ React_Firebase/kakyeboo/src/App.js
@@ -1,11 +1,29 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
