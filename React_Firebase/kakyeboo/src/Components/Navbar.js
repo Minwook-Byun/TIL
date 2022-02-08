e75f485e78842145d60b3f9d4f77326ba367d82f
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-08 16:55:55 +0900

    💄 추가 : 로그아웃 버튼 추가

--- React_Firebase/kakyeboo/src/Components/Navbar.js
+++ React_Firebase/kakyeboo/src/Components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useLogOut } from "../hooks/useLogOut";
+
 // CSS
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { logOut } = useLogOut();
+
   return (
     <>
       <nav className={styles.navbar}>
@@ -12,7 +16,12 @@ const Navbar = () => {
             <Link to="/login">로그인</Link>
           </li>
           <li>
-            <Link to="/login">회원가입</Link>
+            <Link to="/signup">회원가입</Link>
+          </li>
+          <li>
+            <button className="btn" onClick={() => logOut}>
+              로그아웃
+            </button>
           </li>
         </ul>
       </nav>
