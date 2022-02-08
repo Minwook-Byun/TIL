c535afaf366baa8bba9775e5e2736670aab2cf32
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-08 18:49:22 +0900

    ✨ 💄 추가 : 로그인 상태에 따른 조건부 렌더링(라우트가드)

--- React_Firebase/kakyeboo/src/App.js
+++ React_Firebase/kakyeboo/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
@@ -6,24 +7,28 @@ import SignUp from "./pages/SignUp/SignUp";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  {
+    /* 어쓰 상태 기준으로 조건부 렌더링&가드  */
+  }
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route path="/login">
-            <Login />
-          </Route>
-
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady ? (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {user ? <Home /> : <Redirect to="/login" />}
+            </Route>
+            <Route path="/login">
+              {user ? <Redirect to="/" /> : <Login />}
+            </Route>
+            <Route path="/signup">
+              {user ? user.displayName && <Redirect to="/" /> : <SignUp />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      ) : null}
     </div>
   );
 }
